117b20b065d0c8bb24fa9cb574106701040b0b8b
Ce1yo <gbcelyo@example.com>
2025-04-23 17:26:37 +0200

    Mise à jour des fichiers de configuration et des fonctions Netlify

--- firebase-config.js
+++ firebase-config.js
@@ -14,4 +14,9 @@ const firebaseConfig = {
   measurementId: "G-6YRWHECNLK"
 };
 
-/
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, doc, setDoc, getDoc, collection, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut };
